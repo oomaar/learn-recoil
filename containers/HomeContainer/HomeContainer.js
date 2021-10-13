bd0688e6460168625e9d261c5a7ec5b1188292b7
Omar <omaar_5@hotmail.com>
2021-10-13 19:34:03 +0200

    style: HomeContainer

--- containers/HomeContainer/HomeContainer.js
+++ containers/HomeContainer/HomeContainer.js
@@ -1,10 +1,19 @@
 import {
     Container,
+    Nav,
+    BlogLink,
 } from "./styledHomeContainer";
 
 export const HomeContainer = () => {
     return (
         <Container>
+            <Nav>
+                <BlogLink>Blog</BlogLink>
+                <span>/</span>
+                <BlogLink>New</BlogLink>
+                <span>/</span>
+                <BlogLink>Popular</BlogLink>
+            </Nav>
             Posts
         </Container>
     );
--- containers/HomeContainer/styledHomeContainer.js
+++ containers/HomeContainer/styledHomeContainer.js
@@ -1,7 +1,18 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  border: 1px solid red;
   max-width: 1350px;
   margin: 0 auto;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const BlogLink = styled.p`
+  font-weight: bold;
+  font-size: 1.2rem;
+  cursor: pointer;
 `;
